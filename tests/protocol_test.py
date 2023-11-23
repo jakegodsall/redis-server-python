644f30ce602a4f4d041b1beb7ead1a2f2c4e8d38
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-23 10:04:28 +0000

    added parametrised test

--- tests/protocol_test.py
+++ tests/protocol_test.py
@@ -1,3 +1,4 @@
+import pytest
 from dataclasses import dataclass
 
 MSG_SEPARATOR = b"\r\n"
@@ -16,20 +17,13 @@ def extract_frame_from_buffer(buffer):
         
     return None, 0
 
-def test_read_frame_simple_string_incomplete_frame():
-    buffer = b"+Par"
-    frame, frame_size = extract_frame_from_buffer(buffer)
-    assert frame == None
-    assert frame_size == 0
+@pytest.mark.parametrize("buffer, expected", [
+    (b"+Par", (None, 0)),
+    (b"+OK\r\n", (SimpleString("OK"), 5)),
+    (b"+OK\r\n+Next", (SimpleString("OK"), 5)),
+])
 
-def test_read_frame_simple_string_complete_frame():
-    buffer = b"+OK\r\n"
-    frame, frame_size = extract_frame_from_buffer(buffer)
-    assert frame == SimpleString("OK")
-    assert frame_size == 5
+def test_read_frame_simple_string(buffer, expected):
+    actual = extract_frame_from_buffer(buffer)
+    assert actual == expected
 
-def test_read_grame_simple_string_extra_data():
-    buffer = b"+OK\r\n+Next"
-    frame, frame_size = extract_frame_from_buffer(buffer)
-    assert frame == SimpleString("OK")
-    assert frame_size == 5
